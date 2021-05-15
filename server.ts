b25649f66de28c11f91bff0feae89db024e74ad3
chawanghyeon <chawanghyeon@gmail.com>
2021-05-15 22:40:11 +0900

    refactoring

--- server.ts
+++ server.ts
@@ -1,18 +1,19 @@
-import { Application, send } from "./deps.ts";
-import router from "./router.ts";
-import { staticFileMiddleware } from "./middleware/staticFileMiddleware.ts";
+import { Application, send } from './deps.ts';
+import router from './router.ts';
+import { staticFileMiddleware } from './middleware/staticFileMiddleware.ts';
 
 const app = new Application();
 
-app.addEventListener("listen", ({ hostname, port, secure }) => {
-  console.log(
-    `Listening on: ${secure ? "https://" : "http://"}${hostname ??
-      "localhost"}:${port}`,
-  );
+app.addEventListener('listen', ({ hostname, port, secure }) => {
+	console.log(
+		`Listening on: ${secure ? 'https://' : 'http://'}${
+			hostname ?? 'localhost'
+		}:${port}`
+	);
 });
 
-app.addEventListener("error", (evt) => {
-  console.log(evt.error);
+app.addEventListener('error', evt => {
+	console.log(evt.error);
 });
 
 // register some middleware
